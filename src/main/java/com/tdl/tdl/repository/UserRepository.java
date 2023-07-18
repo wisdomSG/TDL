3069466042c35470a5bd091c507827b238e8b949
seulgi <asd0927122@gmail.com>
2023-07-18 20:06:37 +0900

    유저 검색 기능 구현

--- src/main/java/com/tdl/tdl/repository/UserRepository.java
+++ src/main/java/com/tdl/tdl/repository/UserRepository.java
@@ -4,8 +4,11 @@ package com.tdl.tdl.repository;
 import com.tdl.tdl.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
+
+    List<User> findByUsernameContaining(String keyword);
 }
