4d7347051e29b52999f968d63cc9fa6f32d55c6a
NaHyeong <donadio912@naver.com>
2023-07-24 03:43:15 +0900

    게시물 수정/삭제

--- src/main/java/com/tdl/tdl/controller/PostController.java
+++ src/main/java/com/tdl/tdl/controller/PostController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.Objects;
 
 @Controller
 @RequestMapping("/tdl/post")
@@ -28,67 +29,86 @@ public class PostController {
 
     // 게시물 조회
     @GetMapping("")
-    public String getPosts(Model model){
+    public String getPosts(Model model) {
         List<PostResponseDto> post = postService.getPosts();
         model.addAttribute("postList", post);
         return "main";
     }
 
     @GetMapping("/add")
-    public String addPost(){
+    public String addPost() {
         return "addPost";
     }
+
     // 게시물 등록
     @PostMapping(value = "", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public String createPost (@ModelAttribute PostRequestDto requestDto,
-
-                                       @AuthenticationPrincipal UserDetailsImpl userDetails) throws Exception {
+    public String createPost(@ModelAttribute PostRequestDto requestDto,
+                             @AuthenticationPrincipal UserDetailsImpl userDetails) throws Exception {
         log.info(requestDto.getContents());
-        for(int i=0; i<requestDto.getMultipartFiles().size(); i++) {
+        for (int i = 0; i < requestDto.getMultipartFiles().size(); i++) {
             log.info(String.valueOf(requestDto.getMultipartFiles().get(i)));
         }
-        postService.createPost(requestDto, requestDto.getMultipartFiles(), userDetails.getUser());
+        postService.createPost(requestDto, userDetails.getUser());
         return "main";
     }
 
     // 게시물 선택 조회
     @GetMapping("/{id}")
-    public String getSelectPost(@PathVariable Long id, Model model){
+    public String getSelectPost(@PathVariable Long id, Model model) {
         PostResponseDto post = postService.getSelectPost(id);
         model.addAttribute("post", post);
         return "postDetail";
     }
 
-    // 게시물 수정
-    @PatchMapping("/{id}")
     @ResponseBody
-    public PostResponseDto updatePost (
-            @PathVariable Long id, @RequestPart("content") PostRequestDto requestDto,
-            @RequestPart("imgUrl") List<MultipartFile> multipartFiles, @AuthenticationPrincipal UserDetailsImpl userDetails)
-    {
-        return postService.updatePost(id, requestDto, multipartFiles, userDetails.getUser());
+    @GetMapping("/modify/{id}")
+    public String modifyPost(@PathVariable Long id, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        PostResponseDto post = postService.getSelectPost(id);
+        log.info("여기오류>?");
+        log.info(userDetails.getUser().getUsername());
+        if (!Objects.equals(userDetails.getUser().getUsername(), post.getUser().getUsername())) {
+            log.info("오류");
+            throw new IllegalArgumentException("수정불가");
+        }
+        return "성공";
+    }
+
+    @GetMapping("/update/{id}")
+    public String updatePost(@PathVariable Long id, Model model) {
+        PostResponseDto post = postService.getSelectPost(id);
+        log.info(post.getContent());
+        log.info(String.valueOf(post.getPostImageList().size()));
+        model.addAttribute("post", post);
+        return "modifyPost";
+    }
+
+    // 게시물 수정
+    @PatchMapping(value = "/{id}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public String updatePost(
+            @ModelAttribute PostRequestDto requestDto, @PathVariable Long id, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        postService.updatePost(id, requestDto, userDetails.getUser());
+        return "main";
     }
 
     // 게시물 삭제
     @DeleteMapping("/{id}")
     @ResponseBody
     public String deletePost(
-            @PathVariable Long id, @AuthenticationPrincipal UserDetailsImpl userDetails)
-    {
+            @PathVariable Long id, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         return postService.deletePost(id, userDetails.getUser());
     }
 
     // 게시물 좋아요
     @PostMapping("/like/{id}")
     @ResponseBody
-    public String likePost (@PathVariable Long id, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+    public String likePost(@PathVariable Long id, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         return postService.likePost(id, userDetails.getUser());
     }
 
     // 게시물 좋아요 취소
     @DeleteMapping("/like/{id}")
     @ResponseBody
-    public String deleteLikePost (@PathVariable Long id, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+    public String deleteLikePost(@PathVariable Long id, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         return postService.deleteLikePost(id, userDetails.getUser());
     }
 }
--- src/main/java/com/tdl/tdl/dto/PostResponseDto.java
+++ src/main/java/com/tdl/tdl/dto/PostResponseDto.java
@@ -3,6 +3,7 @@ package com.tdl.tdl.dto;
 import com.tdl.tdl.entity.Comment;
 import com.tdl.tdl.entity.Post;
 import com.tdl.tdl.entity.PostImage;
+import com.tdl.tdl.entity.User;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
@@ -12,18 +13,16 @@ import java.util.List;
 @Getter
 public class PostResponseDto {
     private long postId;
-    private String userImage;
-    private String profilename;
     private String content;
     private int likesCount;
     private LocalDateTime createdAt;
+    private User user;
     private List<PostImageResponseDto> postImageList = new ArrayList<>();
     private List<CommentResponseDto> commentList  = new ArrayList<>();
 
     public PostResponseDto(Post post) {
         this.postId = post.getPostId();
-        this.userImage = post.getUser().getUserImage();
-        this.profilename = post.getUser().getProfilename();
+        this.user = post.getUser();
         this.content = post.getContent();
         this.likesCount = post.getLikesCount();
         this.createdAt = post.getCreatedAt();
